ddb8cc0b501ed77eeff70c7a3f9f115b44f5b4eb
realsmile <57916127@qq.com>
2023-03-18 17:49:35 +0800

    1.0.19

--- realsmile/Entity.go
+++ realsmile/Entity.go
@@ -5,20 +5,20 @@ import (
 )
 
 type Base struct {
-	ID         int64      `gorm:"primaryKey;column:id" json:"id"`
-	CreateDate *time.Time `gorm:"comment:创建日期;not null" json:"createDate"`
-	Version    int32      `gorm:"comment:版本号;not null;default:1" json:"version"`
+	ID         int64     `gorm:"primaryKey;column:id" json:"id"`
+	CreateDate time.Time `gorm:"comment:创建日期;not null" json:"createDate"`
+	Version    int32     `gorm:"comment:版本号;not null;default:1" json:"version"`
 }
 
 type Null struct {
 }
 
 type Result[T interface{}] struct {
-	InnerCode int32   `json:"innerCode"`
-	InnerMsg  *string `json:"innerMsg"`
-	Msg       *string `json:"msg"`
-	Data      *T      `json:"data"`
-	Remark    *string `json:"remark"`
+	InnerCode int32  `json:"innerCode"`
+	InnerMsg  string `json:"innerMsg"`
+	Msg       string `json:"msg"`
+	Data      T      `json:"data"`
+	Remark    string `json:"remark"`
 }
 
 type PaginationResult[T interface{}] struct {
