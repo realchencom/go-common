2d0378354030d0999a8199ffb5bdff041d72445b
realsmile <57916127@qq.com>
2023-03-28 12:32:34 +0800

    1.0.38

--- realsmile/Datasource.go
+++ realsmile/Datasource.go
@@ -2,11 +2,9 @@ package realsmile
 
 import (
 	"fmt"
-	"github.com/pkg/errors"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
-	"reflect"
 )
 
 type Datasource struct {
@@ -29,40 +27,26 @@ func GetMylSqlDB() (*gorm.DB, error) {
 		return DB, nil
 	}
 }
-func Saves(DB *gorm.DB, slices interface{}, size int) error {
-	reflectValue := reflect.Indirect(reflect.ValueOf(slices))
-	for reflectValue.Kind() == reflect.Ptr || reflectValue.Kind() == reflect.Interface {
-		reflectValue = reflect.Indirect(reflectValue)
+func Saves[T any](DB *gorm.DB, slices []T, size int) error {
+	//每组大小
+	amount := len(slices)    //总数
+	group := amount / size   //分组
+	surplus := amount % size //余数
+	if surplus != 0 {
+		group++
 	}
-	value := slices.([]interface{})
-	switch reflectValue.Kind() {
-	case reflect.Slice:
-		{
-			//每组大小
-			amount := len(value)     //总数
-			group := amount / size   //分组
-			surplus := amount % size //余数
-			if surplus != 0 {
-				group++
-			}
-			for i := 0; i < group; i++ {
-				var element interface{}
-				if (i + 1) == group {
-					element = value[i*size : i*size+surplus]
-				} else {
-					element = value[i*size : (i+1)*size]
-				}
-				tx := DB.Save(&element)
-				if tx.Error != nil {
-					return tx.Error
-				}
-			}
-			return nil
+	for i := 0; i < group; i++ {
+		var element interface{}
+		if (i + 1) == group {
+			element = slices[i*size : i*size+surplus]
+		} else {
+			element = slices[i*size : (i+1)*size]
 		}
-	default:
-		{
-			return errors.New("目前只支持切片批量入库。")
+		tx := DB.Save(&element)
+		if tx.Error != nil {
+			return tx.Error
 		}
 	}
+	return nil
 
 }
